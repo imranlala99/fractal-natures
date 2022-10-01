ae0a19ebcad49fff22ae92936cd09654e6a45735
carlinooooo <78627335+carlinooooo@users.noreply.github.com>
2022-10-01 13:03:25 -0700

    upload fractal, split to html and js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 //script goes here
+var myCanvas = document.getElementById("my_canvas");
+var ctx = myCanvas.getContext("2d");
+function draw(startX, startY, len, angle, branchWidth) {
+    ctx.lineWidth = branchWidth;
+
+    ctx.beginPath();
+    ctx.save();
+
+    ctx.translate(startX, startY);
+    ctx.rotate(angle * Math.PI/180);
+    ctx.moveTo(0, 0);
+    ctx.lineTo(0, -len);
+    ctx.stroke();
+
+    if(len < 10) {
+        ctx.restore();
+        return;
+    }
+
+    draw(0, -len, len*0.8, angle-15, branchWidth*0.8);
+    draw(0, -len, len*0.8, angle+15, branchWidth*0.8);
+
+    ctx.restore();
+}
+draw(400, 600, 120, 0, 10)
